b8dc43bf860531ac5ad892c25734ec8d8cff73e0
Kevin Sewell <kevin.w.sewell@gmail.com>
2015-05-29 15:16:09 +0200

    Updateing kungfudev.cloud.common.require.local.js to use rawgit

--- src/main/resources/META-INF/resources/common/js/kungfudev.cloud.common.require.local.js
+++ src/main/resources/META-INF/resources/common/js/kungfudev.cloud.common.require.local.js
@@ -2,17 +2,18 @@
     "use strict";
 
     require.config({
-        baseUrl: "resources/",
+        baseUrl: "/resources/",
         paths: {
             "angular": "http://cdn.jsdelivr.net/webjars/org.webjars/angularjs/1.3.15/angular",
             "domReady": "http://cdn.jsdelivr.net/webjars/org.webjars/requirejs-domready/2.0.1/domReady",
             "jquery": "http://cdn.jsdelivr.net/webjars/org.webjars/jquery/2.1.4/jquery.min",
-            "text": "http://cdn.jsdelivr.net/webjars/org.webjars/requirejs-text/2.0.10-3/text"
+            "text": "http://cdn.jsdelivr.net/webjars/org.webjars/requirejs-text/2.0.10-3/text",
+            "js/common/kungfudev.cloud.common": "https://rawgit.com/kungfudev/kungfudev-cloud/master/kungfudev-cloud-common/src/main/resources/META-INF/resources/common/js/kungfudev.cloud.common"
         },
         shim: {
             "angular": {deps: ["jquery"], exports: "angular"}
         },
         urlArgs: "1.0-SNAPSHOT",
-        deps: ["../../../../../../kungfudev-cloud-common/src/main/resources/META-INF/resources/common/js/kungfudev.cloud.common.bootstrap"]
+        deps: ["https://rawgit.com/kungfudev/kungfudev-cloud/master/kungfudev-cloud-common/src/main/resources/META-INF/resources/common/js/kungfudev.cloud.common.require.local"]
     })
 }();
